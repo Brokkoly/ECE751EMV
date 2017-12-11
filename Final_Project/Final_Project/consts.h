2473da1f0a23df1f9d6389c2f5f41f7ec4f017c8
Brokkoly <brendankrull@gmail.com>
2017-12-10 21:15:46 -0600

    c-code final

---
+++ Final_Project/Final_Project/consts.h
@@ -0,0 +1,10 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+#define SIZE 2
+#define K 16;
+#define N 8;
+#define L 16;
+extern const unsigned int bmask_extern[2];
+extern const unsigned int emask_extern[2];
+extern const unsigned int R2_extern[2];
+#endif
---
+++ Final_Project/Final_Project/inputs.c
@@ -0,0 +1,4 @@
+extern unsigned int M_arr[2] = {0x00000000,0x000039af};
+extern unsigned int E_arr[2] = {0x00000000,0x000037a5};
+extern unsigned int C_arr[2] = {0x00000000,0x00000e1e};
+extern unsigned int A_arr[2] = {0x00000000,0x00001739};
